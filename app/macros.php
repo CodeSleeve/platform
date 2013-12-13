8dcea10f076656426ea8d81fd295665e60b8aec5
Kelt Dockins <kelt@dockins.org>
2013-12-13 10:33:04 -0600

    adding macro

--- app/macros.php
+++ app/macros.php
@@ -22,13 +22,39 @@ function active($link, $active = 'active', $inactive = null)
     return in_array($link, $matches) ? $active : $inactive;
 }
 
+HTML::macro('active', 'active');
+
 /*
 |--------------------------------------------------------------------------
-| HTML::active macro
+| show_message_when
 |--------------------------------------------------------------------------
 |
-| If the $link matches one of the active route patterns we search for then
-| we return $active string else we return the $inactive string.
+| This macro spits out a paragraph tag for this $name (e.g. first_name) and
+| if there are errors the paragraph tag has alert and alert-error on it as
+| well as the error message for that specific name. Uses laravel MessageBag from
+| the Validator.
 |
 */
-HTML::macro('active', 'active');
+function show_message_when($name, $errors, $attributes = array())
+{
+    $attributes_string = "";
+    $content_string = "";
+
+    $attributes['class'] = isset($attributes['class']) ?: "";
+    $attributes['class'] .= " $name";
+
+    if ($errors->has($name))
+    {
+        $attributes['class'] .= " alert";
+        $attributes['class'] .= " alert-danger";
+        $content_string = $errors->first($name);
+    }
+
+    foreach ($attributes as $key => $value) {
+        $attributes_string = " $key = \"" . $value . "\""; 
+    }
+
+    return "<div $attributes_string>$content_string</div>";
+}
+
+HTML::macro('show_message_when', 'show_message_when');

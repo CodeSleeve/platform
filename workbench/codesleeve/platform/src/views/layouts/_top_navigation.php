635a35856053dedb448df7677f7825d6a73808af
Kelt Dockins <kelt@dockins.org>
2014-02-25 02:49:04 -0600

    adding some color and stuff

--- workbench/codesleeve/platform/src/views/layouts/_top_navigation.php
+++ workbench/codesleeve/platform/src/views/layouts/_top_navigation.php
@@ -7,7 +7,20 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
         </button>
-        <a class="navbar-brand" href="#">Codesleeve Panel</a>
+        <a class="navbar-brand" href="<?= action("{$namespace}\HomeController@dashboard") ?>">
+            <img src="/assets/platform/images/logo-small.png" style="position: relative; top: -10px;"> 
+            <span class="logo text">
+                <span class="char1">o</span>
+                <span class="char2">d</span>
+                <span class="char3">e</span>
+                <span class="char4">s</span>
+                <span class="char5">l</span>
+                <span class="char6">e</span>
+                <span class="char7">e</span>
+                <span class="char8">v</span>
+                <span class="char9">e</span>
+            </span>
+        </a>
     </div>
     <div class="navbar-collapse collapse">
         <?= render("{$viewpath}::layouts._top_navigation_user", compact('currentUser', 'namespace')) ?>

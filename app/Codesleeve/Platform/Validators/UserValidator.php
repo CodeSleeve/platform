0d5f881379dd23fc9bcf3a6b4e566c3a21651e8e
Kelt Dockins <kelt@dockins.org>
2014-03-01 01:03:11 -0600

    fixing error in validator

--- app/Codesleeve/Platform/Validators/UserValidator.php
+++ app/Codesleeve/Platform/Validators/UserValidator.php
@@ -12,14 +12,14 @@ class UserValidator extends BaseValidator
         $update = [
             'first_name' 		     => 'required|max:256',
             'last_name' 		     => 'required|max:256',
-            'email' 			     => "required|email|unique:platform_users,email,$model->id",
+            'email' 			     => "required|email|unique:users,email,$model->id",
             'password_confirmation'  => 'required_with:password|same:password'
         ];
 
         $create = [
             'first_name'            => 'required|max:256',
             'last_name' 		    => 'required|max:256',
-            'email'     		    => 'required|email|unique:platform_users',
+            'email'     		    => 'required|email|unique:users',
             'password'  		    => 'required|min:8|max:20|alpha_dash',
             'password_confirmation' => 'required|same:password'
         ];
